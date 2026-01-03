9cd0a2977d2e04c956c7d509c47d13d64135a981
reiaha <rheakhimgaudiano00@gmail.com>
2026-01-03 20:22:52 +0800

    update visualization_pkg setup.py for glob usage and entry point adjustment

--- src/visualization_pkg/setup.py
+++ src/visualization_pkg/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 import os
+from glob import glob
 
 package_name = 'visualization_pkg'
 
@@ -8,11 +9,11 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            [os.path.join('resource', package_name)]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/visualization_launch.py']),
-        ('share/' + package_name + '/config', ['config/visualization.rviz']),
+    ('share/ament_index/resource_index/packages',
+        [os.path.join('resource', package_name)]),
+    ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
+    (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -22,8 +23,7 @@ setup(
     license='Apache-2.0',
     entry_points={
         'console_scripts': [
-            'map_node = visualization_pkg.map:main',
+            'map = visualization_pkg.map:main',  # Updated to map.py
         ],
     },
 )
-
